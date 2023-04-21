f691b90ea96515e7498c3def10e385a6e0a4b18d
saaidin <dinme73@gmail.com>
2023-04-21 22:32:22 +0800

    Layout for Hompage ui

---
+++ pages/components/HomeContents.js
@@ -0,0 +1,13 @@
+import Hero from "./Hero"
+import Product from "./Product"
+
+const HomeContents = () => {
+  return (
+    <div>
+      <Hero />
+      <Product />
+    </div>
+  )
+}
+
+export default HomeContents
