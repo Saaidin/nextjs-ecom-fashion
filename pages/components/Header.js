a72d1095372705cf4cc50b0b70b94a3b2d844a53
saaidin <dinme73@gmail.com>
2023-04-21 23:18:36 +0800

    header ui

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,5 +1,25 @@
+import Image from "next/image"
+import Link from "next/link"
+import Logo from "../../public/img/logo.svg"
+import { BsBag } from "react-icons/bs"
+
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header>
+      <div className="container mx-auto flex items-center justify-between h-full">
+        {/* logo */}
+        <Link href={"/"}>
+          <div>
+            <Image src={Logo} alt="Logo" width={40} height={40} />
+          </div>
+        </Link>
+        {/* cart */}
+        <div>
+          <BsBag />
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
