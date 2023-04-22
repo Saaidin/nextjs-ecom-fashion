3f5b878ecec8c229dd37754dbc7714c139b2d3b0
saaidin <dinme73@gmail.com>
2023-04-22 16:34:49 +0800

    done for Product section backend

--- pages/components/Product.js
+++ pages/components/Product.js
@@ -1,12 +1,11 @@
-// import { CartContext } from "../../contexts/CartContext"
-
+import { useContext } from "react"
+import { CartContext } from "../../contexts/CartContext"
 import Image from "next/image"
 import Link from "next/link"
-// import { useContext } from "react"
 import { BsEyeFill, BsPlus } from "react-icons/bs"
 
 const Product = ({ product }) => {
-  // const { addToCart } = useContext(CartContext)
+  const { addToCart } = useContext(CartContext)
 
   const { id, image, category, title, price } = product
 
@@ -25,7 +24,7 @@ const Product = ({ product }) => {
           </div>
         </div>
         <div className="absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
-          <button>
+          <button onClick={() => addToCart(product, id)}>
             <div className="flex justify-center items-center text-white w-12 h-12 bg-red-500">
               <BsPlus className="text-3xl" />
             </div>
